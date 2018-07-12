a27b216d1a5a943d801c37c1e233ae91ca63e408
Jyasveer Gotta <jgotta@redhat.com>
2018-07-12 13:38:51 +0530

    Revert ":fix(telemetry): added telemetry to dep editor"
    This reverts commit 95aaecda00b89abe6958bd3d5b7de8973fcce6da.

--- index.ts
+++ index.ts
@@ -1,4 +1,3 @@
 export { DependencyEditorModule } from './src/app/dependency-editor/dependency-editor.module';
 export { URLProvider } from './src/app/shared/url-provider';
 export { DependencyEditorTokenProvider } from './src/app/shared/depeditor-tokenprovider';
-export { broadcast, StaticInjector } from './src/app/shared/telemetry.decorator';
