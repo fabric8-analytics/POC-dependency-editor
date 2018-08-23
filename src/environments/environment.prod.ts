0737e458ecdba084c4ef21775b86c8d645c3e4dd
invinciblejai <mailjai.vardhan@gmail.com>
2018-08-23 21:26:10 +0530

    fix(env): pick env for OSIO_AUTH_TOKEN dynamically in env files

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   ANALYTICS_RECOMMENDER_URL: 'https://recommender.api.openshift.io/',
   ANALYTICS_LICENSE_URL: 'https://license-analysis.api.openshift.io/',
-  OSIO_AUTH_TOKEN: 'OSIO_AUTH_TOKEN'
+  OSIO_AUTH_TOKEN: '%OSIO_AUTH_TOKEN%'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,7 +6,7 @@ export const environment = {
   production: false,
   ANALYTICS_RECOMMENDER_URL: 'https://recommender.api.openshift.io/',
   ANALYTICS_LICENSE_URL: 'https://license-analysis.api.openshift.io/',
-  OSIO_AUTH_TOKEN: 'OSIO_AUTH_TOKEN'
+  OSIO_AUTH_TOKEN: '%OSIO_AUTH_TOKEN%'
 };
 
 /*
